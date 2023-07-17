6f8204ab02dd38a40653e9409e39db72b53dcc22
Heyboy2004 <113125972+Heyboy2004@users.noreply.github.com>
2023-07-17 11:48:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,7 +240,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        protect_content=True if protect == 'filep' else False,
+        protect_content=True if pre == 'filep' else False,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('channel', url="https://t.me/sk_movies_Group")]])
     )
 
