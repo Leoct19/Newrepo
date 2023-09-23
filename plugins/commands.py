ee555551a844fb2f1b6559534e5fef078668e490
podimonv3 <145585918+podimonv3@users.noreply.github.com>
2023-09-23 19:59:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -40,12 +40,8 @@ async def start(client, message):
             InlineKeyboardButton('➕Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_sticker("CAACAgIAAxkBAAEKWJ9lCw-byUF-lLe6Ou-ZS7lsS2m6bAACTQwAAkkBmEvXMhS3znL1SjAE") 
-        await asyncio.sleep(1)
-        await m.delete()        
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.SUR_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await message.reply_text(          
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
